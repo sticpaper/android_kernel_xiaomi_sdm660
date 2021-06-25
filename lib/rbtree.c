70633fc7c4db99d2794c02f79f886e182c8fefe1
stic-server-open <1138705738@qq.com>
2021-06-25 15:47:08 +0800

    Revert "rbtree: optimize root-check during rebalancing loop"
    This reverts commit a80531c80f5dce2d3b6e004dbcaa8eb25056ce5a.

--- lib/rbtree.c
+++ lib/rbtree.c
@@ -105,25 +105,16 @@ __rb_insert(struct rb_node *node, struct rb_root *root,
 
 	while (true) {
 		/*
-		 * Loop invariant: node is red.
+		 * Loop invariant: node is red
+		 *
+		 * If there is a black parent, we are done.
+		 * Otherwise, take some corrective action as we don't
+		 * want a red root or two consecutive red nodes.
 		 */
-		if (unlikely(!parent)) {
-			/*
-			 * The inserted node is root. Either this is the
-			 * first node, or we recursed at Case 1 below and
-			 * are no longer violating 4).
-			 */
+		if (!parent) {
 			rb_set_parent_color(node, NULL, RB_BLACK);
 			break;
-		}
-
-		/*
-		 * If there is a black parent, we are done.
-		 * Otherwise, take some corrective action as,
-		 * per 4), we don't want a red root or two
-		 * consecutive red nodes.
-		 */
-		if(rb_is_black(parent))
+		} else if (rb_is_black(parent))
 			break;
 
 		gparent = rb_red_parent(parent);
