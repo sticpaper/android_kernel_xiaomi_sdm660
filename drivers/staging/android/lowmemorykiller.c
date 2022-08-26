9a00d204e6bdce68cdadafb32906ec58b21401cf
stic-server-open <1138705738@qq.com>
2022-08-26 08:25:04 +0800

    drivers: android-lmk: Disable dynamic memory killer by default
    * Unexpected effects and frequent device freezes on devices with lower memory
    * And set the modules sysfs node to read-only to prevent changes
    * And change the code comment of adaptive_lmk
    
    Change-Id: I51d4dc6bf12c71b9ec21b3cf4c2fade5e70382c0

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -239,10 +239,9 @@ static short adj_max_shift = 353;
 module_param_named(adj_max_shift, adj_max_shift, short,
                    S_IRUGO | S_IWUSR);
 
-/* User knob to enable/disable adaptive lmk feature */
-static int enable_adaptive_lmk;
-module_param_named(enable_adaptive_lmk, enable_adaptive_lmk, int,
-		   S_IRUGO | S_IWUSR);
+/* User knob to disable adaptive lmk feature */
+static int enable_adaptive_lmk = 0;
+module_param_named(enable_adaptive_lmk, enable_adaptive_lmk, int, S_IRUGO);
 
 /*
  * This parameter controls the behaviour of LMK when vmpressure is in
